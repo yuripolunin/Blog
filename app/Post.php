8bd0682d5fb630b91f672123aa889e27a6dd70fc
Yuri <ff20051@yandex.ru>
2018-10-27 18:08:26 +0300

    Making frontend for admin section
    Making frontend part for admin section using webpack

--- app/Post.php
+++ app/Post.php
@@ -45,3 +45,112 @@ class Post extends Model
             ]
         ];
     }
+
+
+    public static function add($fields)
+    {
+        $post -> new static;
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post-> = save();
+
+        return $post;
+    }
+
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this-> = save();
+    }
+
+    public function remove()
+    {
+        // delete post img
+        Storage::delete('uploads/' . $this->image);
+        // delete post
+        $this->delete();
+    }
+
+    public function uploadImage($image)
+    {
+        if ($image == null) { return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getImage()
+    {
+        if ($this->image == null) {
+            return '/img/no-image.png';
+        }
+        return '/uploads' . $this->image;
+    }
+
+    public function setCategory($id)
+    {
+        if ($id == null) { return; }
+
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function saveTags($ids)
+    {
+        if ($ids == null) { return; }
+
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+        $this->status = Post::IS_DRAFT;
+        $this->save();
+    }
+
+    public function setPublic()
+    {
+        $this->status = Post::IS_PUBLIC;
+        $this->save();
+    }
+
+
+    public function toggleStatus($value)
+    {
+        if ($value == null)
+        {
+            return $this->setDraft();
+        }
+        
+        return $this->setPublic();
+        
+    }
+
+    public function setFeatured()
+    {
+        $this->is_featured = 1;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->is_featured = 0;
+        $this->save();
+    }
+
+
+    public function toggleFeatured($value)
+    {
+        if ($value == null)
+        {
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
+
+    }
+}
